ddbbd58fd5df73d999cc282cb375358ad3ba9b93
Joshua Pelletier <jgpelletier@gmail.com>
2014-03-05 19:43:52 +0000

    LCTHW exercise 1

---
+++ c/LCTHWexercises/ex2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    puts("Hello World"); /*writes the string s and a trailing newline to stdio*/
+    puts("I am live!");
+    puts("Thank you so much for the wonderful introduction.");
+    puts("Let's do this...");
+
+    return 0;
+}
