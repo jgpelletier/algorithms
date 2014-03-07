18b2fe2f33920871660ba901cf9d0ee3b4acaff9
Joshua Pelletier <jgpelletier@gmail.com>
2014-03-07 18:21:46 +0000

    Exercise 5

---
+++ c/LCTHWexercises/ex5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*This is a multi line comment. It will fit as many characters as needed*/
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+
+    //this is another comment but it only goes to the end of the line.
+    printf("You are %d miles away.\n", distance);
+
+    return 0;
+}
