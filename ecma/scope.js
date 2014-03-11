144aa2a23f4ee3100596b5a8b9ae5b416061e09d
Joshua Pelletier <jgpelletier@gmail.com>
2014-03-11 01:13:18 +0000

    Exercise to understand scope

---
+++ ecma/scope.js
@@ -0,0 +1,21 @@
+// implement inc and do not change var a or the parameters to inc.
+//
+function twoify (n) { // scope is local here)
+    n = 'mister steve'
+    console.log(n)
+}
+
+var a = 1
+twoify(a)// this functions scope does not exist outside the function.
+
+console.log(a) // <- this is 1
+
+function inc (n) { // Because n is an argument within the parentheses the scope is local
+   n = function() {
+         n = n + 1
+         }
+   console.log(n)
+}
+
+inc(a)
+console.log(a) // Currently, this is 1. It needs to be 2.
