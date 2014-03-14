08ec1b19776c3c68899b8a3fa779379ae56e1a05
Joshua Pelletier <jgpelletier@gmail.com>
2014-03-14 14:39:46 +0000

    First loop counts backwards

---
+++ c/LCTHWexercises/ex11.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i  = 3;
+    while (i > argc) {
+        printf("arg %d: %s\n", i, argv[i]);
+        i--;
+    }
+
+    char *states[] = {
+        "California", "Oregon",
+        "Washington", NULL
+    };
+
+   int num_states = 4;
+    i = 0;
+    while(i < num_states) {
+        printf("state %d: %s\n", i, states[i]);
+        i++;
+    }
+
+    return 0;
+}
