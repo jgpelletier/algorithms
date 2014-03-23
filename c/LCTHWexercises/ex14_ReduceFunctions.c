dd958e49d278eb710346fc72616f5acecf62a2f8
Joshua Pelletier <jgpelletier@gmail.com>
2014-03-23 07:36:27 -0700

    Update ex14_ReduceFunctions.c

--- c/LCTHWexercises/ex14_ReduceFunctions.c
+++ c/LCTHWexercises/ex14_ReduceFunctions.c
@@ -5,13 +5,13 @@ int main(int argc, char *argv[])
 {
     int i = 0;
 
-    for(i = 1; i < argc; i++) { //<- this is the prints_argument loop
+    for(i = 1; i < argc; i++) { //<- this is the prints_argument function loop
             int j = 0;
 
-            for(j = 0; argv[i][j] != '\0'; j++) { //<- this is the print_letters loop
+            for(j = 0; argv[i][j] != '\0'; j++) { //<- this is the print_letters function loop
                 char ch = argv[i][j];
 
-                if(isalpha(ch) || isblank(ch) || isdigit(ch)) { //<- this is can_print_it
+                if(isalpha(ch) || isblank(ch) || isdigit(ch)) { //<- this is can_print_it function
                    printf("%c == %d ", ch, ch);
                     }
                 printf("\n");
