716881b7c6248829e6711398f76e86ed868e79ac
Joshua Pelletier <jgpelletier@gmail.com>
2014-03-26 08:14:05 +0000

    K & R Exercise 1.1

---
+++ c/TheC_ProgrammingLanguage/chap1/1_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    printf("hello world!\n");
+    return 0;
+}
+
+/* Mistakes:
+ * leave out int from int main
+ * undefined reference
+ *left out ;
+ * warning: expected ;
+
+ * mis-spell printf
+ * warning:recieve error
+ *
+ * enter library incorrectly
+ * warning - fatel error
+ *
+ */
