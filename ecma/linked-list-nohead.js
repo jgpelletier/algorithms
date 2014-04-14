6e572f589a8f749678ced39d26b2468a13f6a479
Joshua Pelletier <jgpelletier@gmail.com>
2014-04-14 03:02:22 +0000

    Implemented 'Push,' 'Pop,' and 'Dump' functions

--- ecma/linked-list-nohead.js
+++ ecma/linked-list-nohead.js
@@ -7,7 +7,7 @@
 // Recall the difference between variables and properties:
 //
 // Variables are numbers, strings, or references. Variables have scope. If a
-// vaiable is declared in a function it is local and exists only within the
+// variable is declared in a function it is local and exists only within the
 // function. Variables without a specified value are undefined.
 //
 // A property is an association between a name and a value/reference that is
@@ -20,7 +20,6 @@ var third = { value: 37, next: { value: 12 } }
 
 function dump (list) { // <- outer scope is true.
     var node = list
-    console.log('list in dump', list === first)
     while (node) {
         if (node) {
             console.log(node.value)
@@ -29,32 +28,28 @@ function dump (list) { // <- outer scope is true.
     }
 }
 
-function push (list, value) { // parameters have local scope, yet list === first TRUE. Cant comment out outer var.
-//  var list // <- redundant
-    var node = {}
-    console.log('list is `first`', list === first) // <- what happens?
-    node.value = value
-    node.next = list  // works if a variable from the outer scope is inserted rather then inner
-    list = node // list appears to inherit the properties of node but only locally
-    if (list.value) {
-        console.log('here is value', list.value) // list.value is true but outer var does not inherit property
-    }
-    return list
+function push (object, value) {
+    return { value: value, next: object }
 }
 
-function pop (list) {
+function pop (object) {
+    var node = object.next
+    return node
 }
 
-
+first = push(first, 12)
+first = push(first, 37) //{ value: 37, next: {value: 12, next: {null}}}
+first = pop(first)
 dump(first)
-//dump(third)
 
-push(first, 12) //list === first <- TRUE
+//dump(third)
+//first = push(37)
+//push(first, 12) //list === first <- TRUE
 //dump(first)
 //push(third, 37) //list === first <- FALSE
 //console.log(first)
-console.log("---push function complete---")
-dump(first) //Does not push 12
+//console.log("---push function complete---")
+//dump(first) //Does not push 12
 //dump(second)
 //console.log(third)
-dump(third) //Does not push 37
+//dump(third) //Does not push 37
