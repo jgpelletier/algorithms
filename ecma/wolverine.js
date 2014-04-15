518ec449197e29347dc0c940f01d19bfafe9b438
Joshua Pelletier <jgpelletier@gmail.com>
2014-04-15 18:03:50 +0000

    Exploring ways to transform array elements to objects.

--- ecma/wolverine.js
+++ ecma/wolverine.js
@@ -1,10 +1,16 @@
 var fs = require('fs')
 var filename = process.argv[2]
+var stops
 
 files = fs.readFile(filename, function(err, data) {
-    var array = data.toString().split('\n')
-    var object = array.toString().split('\,')
+    var array = data.toString().split('\n') //splits lines into array elements
+    var properties = array.toString().split('\,') //splits elements at comma. Need var array for this line to work.
+    var stops = {}
     for( var i = 0; i < array.length - 1; i++) {
-        console.log(object)
+        stops = array[i] //With stops.east the array splits along '/n' into objects. Everything is a poperty of east.
+                              //I want for very specific properties: Station,
+                              //City, State, East.
+
+        console.log(stops)
     }
  });
