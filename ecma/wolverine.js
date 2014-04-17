709dfac91cc2e51d6683bcdd9d7a013166d36c42
Joshua Pelletier <jgpelletier@gmail.com>
2014-04-17 17:02:09 +0000

    Created new issues to work through and dumped the array

--- ecma/wolverine.js
+++ ecma/wolverine.js
@@ -1,4 +1,12 @@
 var fs = require('fs')
 var data = fs.readFileSync(process.argv[2])
-var array = data.toString().split('\n') //splits lines into array elements
-array.forEach(function(element) {console.log(element)})
+// * Read about the `typeof` operator (MDN or other).
+// * Read about the `constructor` object property (MDN or other).
+console.log(typeof data, data.constructor.name)
+// * What is the TYPE of data, where TYPE is not an exacting term?
+// * What other TYPEs can `readFileSync` return?
+// * Where can you find documentation on the specific implementation of `toString`
+//   below?
+// * How can you eliminate the call to `toString`?
+var array = data.toString().split('\n') //
+console.log(array)
