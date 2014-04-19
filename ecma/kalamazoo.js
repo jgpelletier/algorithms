bf07483f229fbe55836077a4d0e4aad7c51423e2
Joshua Pelletier <jgpelletier@gmail.com>
2014-04-19 17:33:08 +0000

    The station function takes a string and returns an array.

--- ecma/kalamazoo.js
+++ ecma/kalamazoo.js
@@ -2,13 +2,22 @@
 var string = "Kalamazoo Transportation, Kalamazoo, Michigan"
 //var kalamazoo = { station: "Kalamazoo Transporation",  city: "Kalamazoo", state: "Michigan" }
 var str = string
+
 function station (string) {
     var string =  string.split("\,")
     return string
 }
 
-console.log(typeof station(string), station(string).constructor.name)
+console.log("string pre-station call is a:", typeof string)
+console.log("Is string an array test:", Array.isArray(string)) //evaluates false
+console.log("str pre- station call is an:", typeof str) //typeof is string
+console.log("Is str an array test:", Array.isArray(str)) //evaluates false
+console.log("------Station function called-------")
+//console.log(typeof station(string), station(string).constructor.name)
 string = station(string)
-console.log(string.toString())
-console.log(string.toString() == str) //These are equal
-console.log(string.toString() == string)
+console.log("Is string an array test:", Array.isArray(string)) //evaluates true
+console.log("string post station call is an:", typeof string) //typeof is an object
+console.log("str post-station call is an:", typeof str) //typeof is string
+console.log("Is str an array test:", Array.isArray(str)) //evaluates false
+
+// This currently changes the string into an array object. I need an object.
