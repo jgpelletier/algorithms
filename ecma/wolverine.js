08524820da63c2dace20be8d0a04a300342771b4
Joshua Pelletier <jgpelletier@gmail.com>
2014-04-22 03:04:53 +0000

    Added two functions to turn an array of string objects into an array of objects

--- ecma/wolverine.js
+++ ecma/wolverine.js
@@ -1,18 +1,21 @@
 var fs = require('fs')
-//var data = fs.readFileSync(process.argv[2])//,'utf8')
 var lines = fs.readFileSync(process.argv[2], 'utf8').split('\n')
-// * Read about the `typeof` operator (MDN or other).
-// * Read about the `constructor` object property (MDN or other).
-//console.log(typeof data, data.constructor.name)
-console.log(typeof lines, lines.constructor.name)
-// * What is the TYPE of data, where TYPE is not an exacting term?
-// * What other TYPEs can `readFileSync` return?
-// * Where can you find documentation on the specific implementation of `toString`
-//   below?
-// * How can you eliminate the call to `toString`?
-//var array = data.toString().split('\n') //
-//console.log(array)
+var pop = lines.pop()
+//console.log(lines)
 
-var o = []
-console.log(typeof o, o.constructor.name)
+function objectFrom(string) {
+    string = string.split(',')
+    var object = { station: string[0].trim(), city: string[1].trim(), state: string[2].trim() }
+    return object
+}
+
+function objectify(array) {
+    for ( i = 0; i < array.length; ++i) {
+        array[i] = objectFrom(array[i])
+    }
+    return array
+}
+
+lines = objectify(lines)
+
 console.log(lines)
