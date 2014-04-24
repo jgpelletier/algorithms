1294b8e1936850b2ae687d2ed7c443cbac5fffbe
Joshua Pelletier <jgpelletier@gmail.com>
2014-04-24 23:24:49 +0000

    Worked on function to create linked list, but it connects no more than one link.

--- ecma/wolverine.js
+++ ecma/wolverine.js
@@ -5,8 +5,8 @@ var pop = lines.pop()
 
 function objectFrom(string) { //converts string into object
     string = string.split(',')
-    var object = { station: string[0].trim(), city: string[1].trim(), state: string[2].trim() }
-    return object
+    var object = { station: string[0].trim(), city: string[1].trim(), state: string[2].trim(), east: object }
+    return object //I am uncertain about adding the property east to the object now or later
 }
 
 function objectify(array) { // converts sting.Object elemen into Object element
@@ -16,15 +16,36 @@ function objectify(array) { // converts sting.Object elemen into Object element
     return array
 }
 
-function push(array) {     // shifts an object from the array so it referenced by the variable node,
-    return array.shift()
+function  link (object, array) {
+    if (!object) {
+        console.log("SexMachine")
+        object = array.shift()
+        return object
+    } else {
+        while (object) {
+            console.log("HotPants")
+            if (!object.east) {
+                console.log("AintItFunkyNow", object)
+                //var node = array.shift()
+                object.east = array.shift()
+                console.log(object)
+                return object
+            }
+            object = object.east
+        //return object
+        }
+    }
 }
-
 
 //simple steps: what would be the most simple thing to do next?
 
 lines = objectify(lines)
-var node = push(lines)
-node = push(lines)
-console.log(node)
-console.log(lines)
+var list = link(list, lines) //SexMachine
+console.log(list)
+//console.log(lines)
+list = link(list, lines)//HotPants AintItFunkyNow
+console.log(list)
+//console.log(lines)
+list = link(list, lines)//HotPants HotPants AintItFunkyNow
+console.log(list)
+//console.log(lines)
