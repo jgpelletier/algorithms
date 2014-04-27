94e3d5b6f2595d384403820fd58aef96673b1614
Joshua Pelletier <jgpelletier@gmail.com>
2014-04-27 23:05:47 +0000

    Clarifying comment.

--- ecma/ascii.js
+++ ecma/ascii.js
@@ -1,4 +1,4 @@
 // Print "substitute": http://www.asciitable.com/
 
-var string = '\"substitute\": http://www.asciitable.com'
+var string = '\u001A: http://www.asciitable.com' //<- unsure of instruction. Please Clarify.
 console.log(string)
