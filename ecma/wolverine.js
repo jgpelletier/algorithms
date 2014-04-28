ac3cda9def07a62687e675e9ef9353184cced0a7
Joshua Pelletier <jgpelletier@gmail.com>
2014-04-28 18:48:16 +0000

    Added dump function.

--- ecma/wolverine.js
+++ ecma/wolverine.js
@@ -1,22 +1,27 @@
 var fs = require('fs')
+var util = require('util')
 var lines = fs.readFileSync(process.argv[2], 'utf8').split('\n')
 var pop = lines.pop()
 
-
-function objectFrom(string) { //converts string into object
+function dump (list) {
+    console.log(util.inspect(list, null, null))
+}
+// spacing
+function objectFrom (string) { //converts string into object
     string = string.split(',')
     var object = { station: string[0].trim(), city: string[1].trim(), state: string[2].trim(), east: object }
     return object //I am uncertain about adding the property east to the object now or later
 }
 
-function objectify(array) { // converts sting.Object elemen into Object element
+// spacing is bad and fix it
+function objectify (array) { // converts string.Object element into Object element.
     for (var i = 0; i < array.length; ++i) {
         array[i] = objectFrom(array[i])
     }
     return array
 }
 
-function  link (object, array) {
+function link (object, array) {
     var list = object
     if (!object) {
         console.log("Chameleon")
@@ -32,46 +37,44 @@ function  link (object, array) {
         return object
     } else if (!object.east.east.east) {
         object.east.east.east = array.shift()
-        console.log(object) //Why does this return as Object? The element is shifted from the array.
+        //console.log(object) //Why does this return as Object? The element is shifted from the array.
         return list
+    } else if (!object.east.east.east.east) {
+        console.log("Sly")
+       object.east.east.east.east = array.shift()
+        return object
+    } else if (!object.east.east.east.east.east) {
+        object.east.east.east.east.east = array.shift()
+        return object
+    } else if (!object.east.east.east.east.east.east) {
+        object.east.east.east.east.east.east = array.shift()
+        return object
+    } else if (!object.east.east.east.east.east.east.east) {
+       object.east.east.east.east.east.east.east = array.shift()
+        return object
+    } else if (!object.east.east.east.east.east.east.east.east) {
+        object.east.east.east.east.east.east.east.east = array.shift()
+        return object
+    } else if (!object.east.east.east.east.east.east.east.east.east) {
+        object.east.east.east.east.east.east.east.east.east = array.shift()
+        return object
+    } else if (!object.east.east.east.east.east.east.east.east.east.east) {
+        object.east.east.east.east.east.east.east.east.east.east = array.shift()
+        return object
+    } else if (!object.east.east.east.east.east.east.east.east.east.east.east) {
+        object.east.east.east.east.east.east.east.east.east.east.east = array.shift()
+        return object
+    } else if (!object.east.east.east.east.east.east.east.east.east.east.east.east) {
+        object.east.east.east.east.east.east.east.east.east.east.east.east = array.shift()
+        return object
+    } else if (!object.east.east.east.east.east.east.east.east.east.east.east.east.east) {
+        object.east.east.east.east.east.east.east.east.east.east.east.east.east = array.shift()
+        return object
+    } else if (!object.east.east.east.east.east.east.east.east.east.east.east.east.east.east) {
+        object.east.east.east.east.east.east.east.east.east.east.east.east.east.east = array.shift()
+        return object
     }
 }
-   // } else if (!object.east.east.east.east) {
-   //     console.log("Sly")
-   //     object.east.east.east.east = array.shift()
-   //     return object
-   // } else if (!object.east.east.east.east.east) {
-   //     object.east.east.east.east.east = array.shift()
-   //     return object
-   // } else if (!object.east.east.east.east.east.east) {
-   //     object.east.east.east.east.east.east = array.shift()
-   //     return object
-   // } else if (!object.east.east.east.east.east.east.east) {
-   //     object.east.east.east.east.east.east.east = array.shift()
-   //     return object
-   // } else if (!object.east.east.east.east.east.east.east.east) {
-   //     object.east.east.east.east.east.east.east.east = array.shift()
-   //     return object
-   // } else if (!object.east.east.east.east.east.east.east.east.east) {
-   //     object.east.east.east.east.east.east.east.east.east = array.shift()
-   //     return object
-   // } else if (!object.east.east.east.east.east.east.east.east.east.east) {
-   //     object.east.east.east.east.east.east.east.east.east.east = array.shift()
-   //     return object
-   // } else if (!object.east.east.east.east.east.east.east.east.east.east.east) {
-   //     object.east.east.east.east.east.east.east.east.east.east.east = array.shift()
-   //     return object
-   // } else if (!object.east.east.east.east.east.east.east.east.east.east.east.east) {
-   //     object.east.east.east.east.east.east.east.east.east.east.east.east = array.shift()
-   //     return object
-   // } else if (!object.east.east.east.east.east.east.east.east.east.east.east.east.east) {
-   //     object.east.east.east.east.east.east.east.east.east.east.east.east.east = array.shift()
-   //     return object
-   // } else if (!object.east.east.east.east.east.east.east.east.east.east.east.east.east.east) {
-   //     object.east.east.east.east.east.east.east.east.east.east.east.east.east.east = array.shift()
-   //     return object
-
-
 //simple steps: what would be the most simple thing to do next?
 
 lines = objectify(lines)
@@ -79,5 +82,15 @@ var list = link(list, lines)
 list = link(list, lines)
 list = link(list, lines)
 list = link(list, lines)
-console.log(list)
-console.log(lines)
+list = link(list, lines)
+list = link(list, lines)
+list = link(list, lines)
+list = link(list, lines)
+list = link(list, lines)
+list = link(list, lines)
+list = link(list, lines)
+list = link(list, lines)
+list = link(list, lines)
+list = link(list, lines)
+list = link(list, lines)
+dump(list)
