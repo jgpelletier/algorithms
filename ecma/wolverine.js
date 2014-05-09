b5439e5f5a16f4f83aa906c0e72c7aa4a6603252
Joshua Pelletier <jgpelletier@gmail.com>
2014-05-09 17:39:53 +0000

    cleaned up comments

--- ecma/wolverine.js
+++ ecma/wolverine.js
@@ -137,34 +137,3 @@ dump(mcrr)
 //lastNode = shift(mcrr)//lastNode holds the shifted object
 //dump(mcrr)
 //console.log(lastNode)
-
-
-
-
-
-//console.log("Get Own Property Description\n", Object.getOwnPropertyDescriptor(mcrr, "east"))
-//console.log("--------------")
-//console.log("Get Own Property Name\n", Object.getOwnPropertyNames(mcrr))
-//console.log("--------------")
-//console.log("Get Prototype of\n", Object.getPrototypeOf(mcrr))//{}
-//console.log("--------------")
-//console.log("Is Extensible\n", Object.isExtensible(mcrr))//true
-//console.log("--------------")
-//console.log("Is Frozen\n", Object.isFrozen(mcrr))//false
-//console.log("--------------")
-//console.log("Is Sealed\n", Object.isSealed(mcrr))//false
-//console.log("--------------")
-//console.log("Keys\n", Object.keys(mcrr)) //['station', 'city', 'state', 'east' ]
-//console.log("--------------")
-//console.log("Has Own Property\n", mcrr.hasOwnProperty("east"))//true
-//console.log("--------------")
-//console.log("Is Prototype Of\n", Object.isPrototypeOf(mcrr))//false
-//console.log("--------------")
-//console.log("Is Enumerable\n", Object.propertyIsEnumerable("city")) //false
-//console.log("--------------")
-//console.log("To Locale String\n", mcrr.toLocaleString())
-//console.log("--------------")
-//console.log("To String\n", mcrr.toString())
-//console.log("--------------")
-//console.log("Value Of \n", mcrr.valueOf())//returns primative value: object
-//dump(mcrr)
