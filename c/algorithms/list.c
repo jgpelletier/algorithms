129bbd540c3c10aefc0bb9aeaeeb0468af2170f7
Joshua Pelletier <jgpelletier@gmail.com>
2014-05-17 20:56:40 +0000

    intial commit of list program

---
+++ c/algorithms/list.c
@@ -0,0 +1,13 @@
+/* create a list node structure to store ints.  */
+
+int main ()
+{
+    /* create a linked-list with a head node and one element numbered 5. mallock
+     * teh head node and the one data node. */
+
+    /* print the value of every node in the list. one node at a time. one node
+     * on a line. */
+
+    /* free all nodes. */
+    return 0;
+}
