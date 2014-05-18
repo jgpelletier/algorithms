27f0b6c00c9077a320bdb2c77468ccecfdac858a
Joshua Pelletier <jgpelletier@gmail.com>
2014-05-18 18:00:18 +0000

    added node struct

--- c/algorithms/list.c
+++ c/algorithms/list.c
@@ -1,9 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 /* create a list node structure to store ints.  */
+struct node {
+    int value;
+    struct node* next;
+};
+
+struct node *create_list()
+{
+    node *list;
+    list = malloc(sizeof(node));
+    list->value = 0;
+    list->next = NULL;
+    return list;
+}
 
 int main ()
 {
-    /* create a linked-list with a head node and one element numbered 5. mallock
-     * teh head node and the one data node. */
+    /* create a linked-list with a head node and one element numbered 5. malloc
+     * the head node and the one data node. */
 
     /* print the value of every node in the list. one node at a time. one node
      * on a line. */
