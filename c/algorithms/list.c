315c5bc5fa61ce3d01949d08ff0577fba8006372
Joshua Pelletier <jgpelletier@gmail.com>
2014-05-19 17:47:54 +0000

    initilize head, add one node and print node value.

--- c/algorithms/list.c
+++ c/algorithms/list.c
@@ -8,7 +8,18 @@ struct node {
     struct node* next;
 };
 
-struct node *create_list(int value)
+struct node *head, *z, *t; //variables and type
+
+//algorithms in C list_initialization with head
+/*list_initialize()
+{
+    head = (struct node *) malloc(sizeof *head);//call to malloc creates node
+    z = (struct node *) malloc(sizeof *z);
+    head->next = z; z->next = z;
+}*/
+
+//this is a intializes the list.
+struct node *create_list(struct node* next, int value)
 {
     struct node *list =  malloc(sizeof(struct node));
     assert(list != NULL);
@@ -23,9 +34,17 @@ int main ()
     /* create a linked-list with a head node and one element numbered 5. malloc
      * the head node and the one data node. */
 
+    head = (struct node *) malloc(sizeof *head);//call to malloc creates node
+    z = (struct node *) malloc(sizeof *z);
+    head->next = z;
+    z->value = 5;
+
     /* print the value of every node in the list. one node at a time. one node
      * on a line. */
-
+    while (head->next) {
+    printf("value: %d\n", head->next->value);
+    head = head->next;
+    }
     /* free all nodes. */
     return 0;
 }
