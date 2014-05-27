d8668a4f0db5edb895aeaa900a681144378e929a
Joshua Pelletier <jgpelletier@gmail.com>
2014-05-27 22:55:41 +0000

    only declaring node and head_node in list.

--- c/algorithms/list.c
+++ c/algorithms/list.c
@@ -1,6 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/*
+struct node* list_create ()
+{
+}
+
+void list_delete (struct node* list)
+{
+}
+
+void list_push (struct node* list)
+{
+}
+
+void list_print (struct node* list)
+{
+}
+*/
+
 int main ()
 {
     /* create a list node structure to store ints.  */
@@ -13,45 +31,43 @@ int main ()
 
    /* create a linked-list with a head node and one element numbered 5. malloc
      * the head node and the one data node. */
-    struct node *head_node;
+    struct node *head_node; // <- declaration
     head_node = malloc(sizeof (struct node));//call to malloc creates node
     head_node->value = 0;
     head_node->next = NULL;
     struct node *list;
     list = head_node; /* <- this is the head node. don't lose it,*/
 
-    struct node *first;
-    first = malloc(sizeof (struct node));//second call to malloc
-    first->value = 5;
-    first->next = list->next;
-    list->next = first;
+    struct node *node; // <- declaration
+    node = malloc(sizeof (struct node));//node call to malloc
+    node->value = 5;
+    node->next = list->next;
+    list->next = node;
 
-    struct node *second;
-    second = malloc(sizeof (struct node));//second call to malloc
-    second->value = 15;
-    second->next = first -> next;
-    first->next = second;
+    node = malloc(sizeof (struct node));//node call to malloc
+    node->value = 15;
+    node->next = list->next;
+    list->next = node;
 
 
     /* print the value of every node in the list. one node at a time. one node
      * on a line. */
-    while (list->next) {
-    printf("value: %d\n", list->next->value);
-    list = list->next;
+    node = list;
+    while (node->next) {
+    printf("value: %d\n", node->next->value);
+    node = node->next;
     }
 
-    list = head_node;
     /* free all nodes. */
 
     /* head_node is my list */
     /* this is only releasing one block of memory*/
     do { // we always know we have at least one; head node.
-       struct node *node;
        node = list; // <- list is the head of your list
        list = list->next;
        free(node);
        //free(list);
     } while (list);
 
-  return 0;
+    return 0;
 }
