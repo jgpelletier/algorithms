4c6a5fa88d69b5c8e835cff8b53438cd93447644
Joshua Pelletier <jgpelletier@gmail.com>
2014-05-30 23:12:08 +0000

    header file example.

---
+++ c/algorithms/compile/compile.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+static int i = 0; // <- static is local to `compile.c`.
+
+char greeting[] = "Hello, World!\n";
+
+int* get_number(const char* message) // <- definition
+{
+    printf(message);
+    return &i;  // <- body
+}
