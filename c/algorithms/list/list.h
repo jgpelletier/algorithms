eeda3a9d07d480a4d02667b570ec1f15b29c55f3
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-01 22:51:49 +0000

    header file for linked list functions.

---
+++ c/algorithms/list/list.h
@@ -0,0 +1,16 @@
+struct node {//declaration <- nothing extern about it.
+    int value;
+    struct node* next;
+}; //"node;" which followed the } was deleted. Typedef was used instead. see below.
+
+typedef struct node node_t; //this is the allocation of memory
+
+struct node *list_create(void); //declaration
+    // ^^^  that's right
+
+void list_delete (node_t *); //declaration
+                // ^^^ same as `struct node`
+
+void list_push (struct node*, int); //declaration.
+
+void list_print (struct node*); //declaration
