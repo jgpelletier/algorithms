a02c6fdc5c0af881b0b8269fc99f80b8f0d0bc3e
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-01 15:19:21 +0000

    line_count function opens a file, reads it to a buffer, prints the buffer, and closes the file.

--- c/algorithms/lines/lines.c
+++ c/algorithms/lines/lines.c
@@ -5,7 +5,7 @@
 int line_count (const char* fname)
 {
     char buffer[1024];
-    int  lines, ch;
+    int  lines;//count;
     lines = 0;
 
     // open the file fname.
@@ -13,21 +13,25 @@ int line_count (const char* fname)
     f =  fopen (fname, "r");//REMEMBER:in c ' ' is very different than " "
 
     // read the file into a buffer of 1024 bytes
-    fread(buffer, 1024 , 1, f);
-    do {
+    fread(buffer, 1024 , 1, f);//
+
+    /*do {
     ch = fgetc(buffer);
     if ( ch == '\n') lines++;
     } while ( ch != EOF);
+    //
     // you will then go through the buffer counting every `\n`.
-    /*for (i = 0; i < count; i++) {//for?  count not declared
-        if (buffer[i] == '\n') lines++;
-    }*/
+
+    //count =
+
+    //for (i = 0; i < *what goes here* buffer ; i++) {*/
+    //    if (buffer[i] == '\n') lines++;
+    // }
+
     // close the file*/
+    printf("%s\n", &buffer[10]);
+
     fclose(f);
 
-
-
-
-
-    return lines; //DONT FORGET TO CHANGE THIS BACK
+    return lines;
 }
