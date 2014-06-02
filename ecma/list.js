59a0d0ed2d6bf92cca82a6c2138dc4cfbd44b0ba
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-02 21:33:11 +0000

    defined an undefined variable in list.js and fixed formatting.

--- ecma/list.js
+++ ecma/list.js
@@ -1,3 +1,55 @@
 // move linked list function, export, and use from railway.
-function linkedList (lines) {
+
+function objectFrom (string) {
+    var object = null// <- what is this?
+    string = string.split(',')
+    return {
+        station: string[0].trim(),
+        city: string[1].trim(),
+        state: string[2].trim(),
+        east: object // <- what is this?
+    }
 }
+
+function objectify (array) {
+    for (var i = 0; i < array.length-1; ++i) {
+        array[i] = objectFrom(array[i])
+    }
+    return array
+}
+
+function link (list, array) {
+    var newNode = array.pop()
+    if (!list) {
+        list =  newNode
+    } else {
+        newNode.east = list
+    }
+    return newNode
+}
+
+function linkedList (array) {
+    var count = array.length
+    var lines = objectify(array)
+    for (var i = 0; i < count; i++) {
+        var list = link(list, lines)
+    }
+    return list
+}
+
+function addWest (list) { // <- simple, west of
+    var node = list
+    var prev
+    while (node.east) {
+        prev = node
+        node = node.east
+        node.west = prev
+    }
+    return list
+}
+
+exports.objectFrom = objectFrom
+exports.objectify = objectify
+exports.link = link
+exports.linkedList = linkedList
+exports.addWest = addWest
