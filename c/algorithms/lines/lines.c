5bf3b78d5ae3177518f3e93e2679c77eee4f5037
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-03 19:34:11 +0000

    fixed control flow and corrected some errors in list.c

--- c/algorithms/lines/lines.c
+++ c/algorithms/lines/lines.c
@@ -15,28 +15,26 @@ int line_count (const char* fname) // <- 10
 
     // open the file fname.
     FILE *f; // FILE is the data type used to represent streams. f is a pointer to that file.
-    if (( f = fopen (fname, "r")) != NULL )
-    {
-        length = fread(buffer, 1, sizeof(buffer), f);// count/size issue
-        if (length) {
+    if (( f = fopen (fname, "r")) != NULL ) {
+        length = fread(buffer, 1, sizeof(buffer), f); // How do I test if buffer is big enough?
+                                                      //   ^^^ this is next
+        if (length > 0) { // <- this test definately wrong
             for (i = 0; i < length; i++) {
                 if (buffer[i] == '\n') lines++; // <- don't change this
-                }
-            if (fclose(f))
-                perror("fclose error");
-            else
+            }
+            if (fclose(f) == 0)
                 printf("file closed successfully\n");
+            else
+                perror("fclose error");
             return lines;
-        }
-        else {
+        } else {
             if (ferror(f))
-                printf("Errno = %d\n", errno);
+                printf("Errno = %d\n", errno);// how do I return the error msg and not the #?
             else if (feof(f))
-                //perror( "EOF found");
                 printf("Errno = %d\n", errno);
         }
-    }
-    else
+    } else {
         printf("fopen failed, errno = %d\n", errno);
-        return -1;
+    }
+    return -1; // <- guarded by the else?
 }
