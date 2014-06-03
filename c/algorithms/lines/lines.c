9c6379109effdaa8b8991445097ba419c336377e
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-03 18:21:22 +0000

    added errno.h and error handling to list.c

--- c/algorithms/lines/lines.c
+++ c/algorithms/lines/lines.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <string.h>
+//#include <string.h>
+#include <errno.h>
 #include "lines.h"
 
 // Fix, then `#define BUFFER_SIZE 16`.
@@ -14,24 +15,28 @@ int line_count (const char* fname) // <- 10
 
     // open the file fname.
     FILE *f; // FILE is the data type used to represent streams. f is a pointer to that file.
-    f = fopen (fname, "r"); // REMEMBER: in c ' ' is very different than " "
-    // ^^^ ERROR? <-> SO MANY ERRORS.
-
-    // read the file into a buffer of 1024 bytes
-    length = fread(buffer, 1, sizeof(buffer), f);// count/size issue
-    // ^^^ ERROR?
-    check for eof or error, if error return -1
-    // you will then go through the buffer counting every `\n`.
-    for (i = 0; i < length; i++) {
-        if (buffer[i] == '\n') lines++; // <- don't change this
+    if (( f = fopen (fname, "r")) != NULL )
+    {
+        length = fread(buffer, 1, sizeof(buffer), f);// count/size issue
+        if (length) {
+            for (i = 0; i < length; i++) {
+                if (buffer[i] == '\n') lines++; // <- don't change this
+                }
+            if (fclose(f))
+                perror("fclose error");
+            else
+                printf("file closed successfully\n");
+            return lines;
+        }
+        else {
+            if (ferror(f))
+                printf("Errno = %d\n", errno);
+            else if (feof(f))
+                //perror( "EOF found");
+                printf("Errno = %d\n", errno);
+        }
     }
-
-    // close the file*/
-
-    ///printf("%s\n %d\n %d\n", &buffer[0], length, sizeof(length));
-
-    fclose(f);
-    // ^^^ ERROR?
-
-    return lines;
+    else
+        printf("fopen failed, errno = %d\n", errno);
+        return -1;
 }
