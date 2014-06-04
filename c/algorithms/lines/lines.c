0c67f06a7b359845d170ff8166b07a1215afaa5f
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-04 22:02:09 +0000

    need to address questions and first if condition.

--- c/algorithms/lines/lines.c
+++ c/algorithms/lines/lines.c
@@ -9,32 +9,35 @@
 int line_count (const char* fname) // <- 10
 {
     char buffer[BUFFER_SIZE];
-    size_t i, length;//count;
+    size_t i, length;// size_t is the preferred way to declare variables that hold the size of an object.
     int  lines;
     lines = 0;
 
     // open the file fname.
     FILE *f; // FILE is the data type used to represent streams. f is a pointer to that file.
     if (( f = fopen (fname, "r")) != NULL ) {
-        length = fread(buffer, 1, sizeof(buffer), f); // How do I test if buffer is big enough?
-                                                      //   ^^^ this is next
-        if (length > 0) { // <- this test definately wrong
+        length = fread(buffer, sizeof(char), sizeof(buffer), f); // How do I test if buffer is big enough?
+        //if (length) { // <- on the spectrum of wrong this is really wrong
+        //if (length > 0) { // <- on the spectrum of wrong this is really wrong
+        // This was the question: How do you know that an error DID NOT occur?
+        if (sizeof(length) == sizeof(int)) { // <- on the spectrum of wrong where does this test fall?WRONG
+       // if (length >= sizeof(char)) { // <- on the spectrum of wrong where does this test fall?
+
             for (i = 0; i < length; i++) {
                 if (buffer[i] == '\n') lines++; // <- don't change this
             }
-            if (fclose(f) == 0)
-                printf("file closed successfully\n");
-            else
+            if (fclose(f) != 0)
                 perror("fclose error");
-            return lines;
+            else
+                return lines;
         } else {
             if (ferror(f))
-                printf("Errno = %d\n", errno);// how do I return the error msg and not the #?
+                printf("Error, Errno = %d\n", errno); // how do I return the error msg and not the #?
             else if (feof(f))
-                printf("Errno = %d\n", errno);
+                printf("EOF, Errno = %d\n", errno);
         }
     } else {
-        printf("fopen failed, errno = %d\n", errno);
+        printf("fopen failed, errno = %d\n", errno /* <- not "thrown" */);
     }
-    return -1; // <- guarded by the else?
+    return -1; // <- guarded by the else? This is your error.
 }
