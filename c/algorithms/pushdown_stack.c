d0cae84861fb58a02861b7165e7dce87f18b8576
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-06 23:01:16 +0000

    update

--- c/algorithms/pushdown_stack.c
+++ c/algorithms/pushdown_stack.c
@@ -18,33 +18,38 @@ void stackinit()
 
 void push(int v)
 {
-
     t = (struct node *) malloc(sizeof *t);
-    t->key = v; t->next = head->next;
+    t->key = v; t->next = head->next;// adds node to the head
     head->next = t;
 }
 
 int pop()
 {
     int x;
-    t = head->next; head->next = t->next;
-    x = t->key;
-    free(t);
-    return x;
+    t = head->next; head->next = t->next;//subtracts a node from the head.
+    x = t->key;//data stored in node
+    free(t);// node is freed
+    return x;// new int available to push on the stack
 }
 
-int stackempty()
+int stackempty()//is this only a test or are the 2 blks in use at exit in hear?
 {
     return head->next == z;
 }
 
+void delete_stack()//not in Sedegwick code.
+{
+    free(struct z);
+    free(head);
+}
+// how do you release all blks of memory
 int main()
 {
     stackinit();
     push(5);
     push(9);
     push(8);
-    push(pop()+pop());
+    push(pop()+pop());//each pop returns an int, which is pushed on the stack
     push(4);
     push(6);
     push(pop()*pop());
@@ -53,7 +58,8 @@ int main()
     push(pop()+pop());
     push(pop()*pop());
     printf("%d\n", pop());
-    stackempty();
+    //stackempty();
+    //delete_stack();
     return 0;
 
 
