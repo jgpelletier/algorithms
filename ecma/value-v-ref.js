31d1877f363cf2c2b0345b5babc358d48e634804
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-06 21:30:53 +0000

    update

--- ecma/value-v-ref.js
+++ ecma/value-v-ref.js
@@ -1,16 +1,22 @@
 var x = { a: 1 }
 var y = x
 
+console.log(x.a) //<- prints 1
+
 y.a = 2
 
-console.log(x.a)
+console.log(x.a) //<- prints 2
+
+x.a = 1
+
+console.log(y.a) //<- prints 1
 
 var x = 1
 var y = x
 
-console.log(y)
+console.log(y) //<-prints 1
 
 y = 2
 
-console.log(x)
-console.log(y)
+console.log(x) //prints 1
+console.log(y) //prints 2
