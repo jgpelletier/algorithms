1452b5b25c1be184e64b59539547261a4f927bac
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-11 21:36:18 +0000

    updated header file to hold struct and so functions return structs

--- c/algorithms/lines/lines.h
+++ c/algorithms/lines/lines.h
@@ -1,2 +1,8 @@
+struct file_info {
+    int lines;
+    size_t length;
+};
 
-int line_count (const char* fname);
+typedef struct file_info file_info_t;
+struct file_info *file_info_create ();
+struct file_info *line_count (const char* fname);
