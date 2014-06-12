c0680faa4bfeff6a5117c26dde46ae610f652c49
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-12 22:36:00 +0000

    added free

--- c/algorithms/lines/main.c
+++ c/algorithms/lines/main.c
@@ -7,5 +7,6 @@ int main ()
     struct file_info *info;
     info = line_count("wolverineX2.txt");
     printf("lines: %d length:%d\n", info->lines, info->length);
+    free(info);
     return EXIT_SUCCESS;
 }
