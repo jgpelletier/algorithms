462da1a22d29df0a001f88408dd5650466969d4a
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-13 17:16:18 +0000

    changed printf

--- c/algorithms/lines/main.c
+++ c/algorithms/lines/main.c
@@ -6,7 +6,7 @@ int main ()
 {
     struct file_info *info;
     info = line_count("wolverineX2.txt");
-    printf("lines: %d length:%d\n", info->lines, info->length);
+    printf("If any value is negative then an error occured: error: %d lines: %d length:%d\n", info->error, info->lines, info->length);
     free(info);
     return EXIT_SUCCESS;
 }
