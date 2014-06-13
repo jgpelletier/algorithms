a282f013d342b530a33eab401613804305eaf6a4
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-13 17:15:47 +0000

    added error member to struct

--- c/algorithms/lines/lines.h
+++ c/algorithms/lines/lines.h
@@ -1,8 +1,9 @@
 struct file_info {
     int lines;
     size_t length;
+    int error;
 };
 
 typedef struct file_info info;
-struct file_info *share_info (int lines, size_t length);
+struct file_info *share_info (int lines, size_t length, int error);
 struct file_info *line_count (const char* fname);
