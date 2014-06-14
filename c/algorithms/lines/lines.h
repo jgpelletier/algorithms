bd8bf07850066f4ef90058856bf0ee93d89954cc
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-14 16:35:28 +0000

    declared new functions.

--- c/algorithms/lines/lines.h
+++ c/algorithms/lines/lines.h
@@ -4,6 +4,17 @@ struct file_info {
     int error;
 };
 
+
+struct file_info2 {
+    int lines;
+    size_t length;
+};
+
+
+
 typedef struct file_info info;
+typedef struct file_info2 info2;
 struct file_info *share_info (int lines, size_t length, int error);
+struct file_info2 *share_info2 (int lines, size_t length);
 struct file_info *line_count (const char* fname);
+void line_count_2 (const char* fname, struct file_info2** info2, int* error);
