74549fe315eb320173f0f0b76646e9a04cc2a9f3
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-16 20:34:55 +0000

    cleaned directory

--- c/algorithms/compile/main.c
+++ c/algorithms/compile/main.c
@@ -6,6 +6,5 @@ int main()
 {
     printf(greeting); // <- greeting as function pointer
     int *pi = get_number(greeting); //returns an int
-    /**pi = inc_number(*pi); // what was this supposed to be*/
     return *pi;
 }
