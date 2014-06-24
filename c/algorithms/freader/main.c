60a833c2688de2138f861cbd807ca00de2685dd0
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-24 19:40:41 +0000

    freader file

---
+++ c/algorithms/freader/main.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+
+
+int main()
+{
+    char buffer[2];
+    FILE* file;
+    size_t length;
+
+    file = fopen("file.txt", "r");
+
+    length = fread(buffer, sizeof(char), sizeof(buffer), file);
+    printf("length: %zu\n", length);
+    printf("full?: %zu\n", length == sizeof(buffer));
+    printf("ferror(file): %d\n", ferror(file));
+
+    fclose(file);
+
+    return EXIT_SUCCESS;
+}
