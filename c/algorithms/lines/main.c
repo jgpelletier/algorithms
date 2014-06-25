cf0e6517264173ddc9ea651cd32d059587f26a5b
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-25 14:28:37 +0000

    removed comments

--- c/algorithms/lines/main.c
+++ c/algorithms/lines/main.c
@@ -15,14 +15,10 @@ void call_line_count_2 ()
 {
     int error = 0;
     struct _file_info2* info2;
-        // ^^^ raw material for 12 different API calls.
-        //          words, words, words
-
-    line_count_2("wolverineX2.txt", &info2, &error); //&info2 is being passed as a pointer to a pointer
+    line_count_2("wolverineX2.txt", &info2, &error);
     printf("If any value is negative then an error occured."
 	   " error: %d lines: %d length: %d\n", error, info2->lines, info2->length);
- // try a different way.
-    free(info2);    // <- why is this necessary?
+    free(info2);
 }
 
 void call_line_count_3 ()
@@ -45,7 +41,7 @@ void call_line_count_4 ()
     lines = length = error = 0;
     info = share_info3(lines, length);
     line_count_3 ("wolverineX2.txt", &info, &error);
-    printf("call_line_count_4: If any value is negative then an error occured. "
+    printf("If any value is negative then an error occured. "
         "error: %d lines: %d length: %d\n", error, info.lines, info.length);
 }
 
