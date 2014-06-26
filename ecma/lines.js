33e45b30951e4579ac881918da2c948230b88ff7
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-26 19:08:38 +0000

    update

--- ecma/lines.js
+++ ecma/lines.js
@@ -1,15 +1,15 @@
 var fs = require('fs')
 
 var buffer = fs.readFileSync('wolverine.txt', 'ascii')
-
+    // ^^ String
 var i = 0
 var line_count = 0
 
 
-for ( i; i < buffer.length; i++) {
+for (i; i < buffer.length; i++) {
     if (buffer[i] == '\n') {
         line_count++
     }
 }
 
-console.log("line count:", line_count, "\ncharacter count:", i)
+console.log("line count: ", line_count, "\ncharacter count: ", i)
