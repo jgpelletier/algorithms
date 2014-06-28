3e67a687a1e050cb7d985b3edab66b4c0541a57c
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-28 03:08:43 +0000

    update

--- ecma/inc.js
+++ ecma/inc.js
@@ -1,17 +1,33 @@
+// Remove all function invocations that provide parameters that are ignored.
 function inc (number) {
-    return number + 1
+    return ++number
 }
 
-var number = inc(2)
+var number
 
-console.log(number)
+number = inc(2)
 
+console.log(number)  // <- 3
+
+number = inc(2)
+
+console.log(number)  // <- 3
+
+
+//what about scope?
 function inker (number) {
     return function () {
-        return number + 1
+        return ++number
     }
 }
 
+// create new variable called number
 var number = inker(2)
+//               ^^^ this argument is entered on line 10
 
-console.log(number(1)) // <- 3
+// the parans evoke the function 'number.'
+// the function 'number' uses inkers parameter, named 'number;'
+// it is visibile throughout the function.
+console.log(number()) // <- 3
+
+console.log(number()) // <- 3, why is this 4?
