6cba8c7ebd59b04a39fcbde34228d4aed64d4c70
Joshua Pelletier <jgpelletier@gmail.com>
2014-06-29 23:07:18 +0000

    examples added

--- ecma/inc.js
+++ ecma/inc.js
@@ -3,6 +3,8 @@ function inc (number) {
     return ++number
 }
 
+var increment = inc
+
 var number
 
 number = inc(2)
@@ -13,13 +15,23 @@ number = inc(2)
 
 console.log(number)  // <- 3
 
+number = increment(2)
+
+console.log(number)  // <- 3
+
+// ^^^ kinda like C
+
 
 //what about scope?
 function inker (number) {
-    return function () {
-        return ++number
+            // ^^^ parmaeters
+    var by = 1 // <- variables
+    return function () { // <- closure DOES NOT EXIST IN C.
+        return number += by
     }
 }
+// higher level, from LISP, find in Perl, Ruby, Python.
+//                  generally interpreted
 
 // create new variable called number
 var number = inker(2)
@@ -33,3 +45,45 @@ console.log(number()) // <- 3
 console.log(number()) // <- 3, why is this 4?
 
 console.log(number()) // <- 5 how come this increments
+
+var number = inker(2)
+
+console.log(number()) // <- 3
+
+var value = 2
+
+var number = inker(value)
+
+console.log(number()) // <- 3
+
+++value
+++value
+++value
+
+console.log(number()) // <- 4
+        // maintains state, changes
+
+function multiply (by) {
+    return function (value) {
+        return value * by
+    }
+}
+
+var byTwo = multiply(2)
+  // ^^^ pure functions, deterministic.
+console.log(byTwo(3)) // <- 6
+
+console.log(number()) // <- 5
+console.log(number()) // <- 6
+
+
+console.log(byTwo(3)) // <- 6 determined by argument, deterministic
+console.log(byTwo(3)) // <- 6 pure function f(x)
+console.log(byTwo(3)) // <- 6
+console.log(byTwo(4)) // <- 8
+console.log(multiply(4)(6)) // <- 20
+
+console.log([ 1, 2, 3, 4, 5, 6, 7 ].map(byTwo))
+            // ^^^ four different machines
+            //
+            // mapped and reduced
