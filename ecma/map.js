2a5e1f116f1827b0bdca1aade1bd6c30b3b8a2f6
Joshua Pelletier <jgpelletier@gmail.com>
2014-07-04 19:21:29 +0000

    clarified notes

--- ecma/map.js
+++ ecma/map.js
@@ -4,14 +4,20 @@ var list = require('./list')
 var lines = fs.readFileSync(process.argv[2], 'utf8').split('\n')
 var railway = list.linkedList(lines)
 
-
 console.log(list.map(railway, function (station) {
+//                                ^^^ this function is passed  an object —-
+//                                    created during the while loop -- as the map
+//                                    function traverses the linked list.
     return station.city + ', ' + station.state
-}))
+})) //^^^ The anonymous function  returns the object's properties
+//        to the map function's lexical environment. This is also
+//        called the scople chain.
 
 
 console.log(list.map(railway, function (state) {
+//                              ^^^ the anonymous function's lexical environment
+//                                  is currently between the {}. It has
+//                                  access to the variables in the outer function,
+//                                  whereas its environment is hidden.
     return state.city + ', ' + state.station + ', ' + state.state
 }))
-
-//console.log(typeof mapped()) //<- with () evaluates object, without function*/
