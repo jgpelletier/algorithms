e71af4e7f40b32045285d1ff4772a6588bfb18d6
Joshua Pelletier <jgpelletier@gmail.com>
2014-07-08 21:12:41 +0000

    LCTHW ex17 shell script

---
+++ c/LCTHWexercises/ex17.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+echo "compiling code"
+make clean
+make ex17
+
+echo "Creating Database"
+./ex17 db.dat c
+
+echo "Setting Records"
+./ex17 db.dat s 1 John John@beatles.com
+./ex17 db.dat s 2 Peter Peter@beatles.com
+./ex17 db.dat s 3 Ringo Ringo@beatles.com
+./ex17 db.dat s 4 Paul Paul@beatles.com
+./ex17 db.dat s 5 George George@beatles.com
+
+echo "Retrieving record 2"
+./ex17 db.dat g 2
+
+echo "Deleting record 2"
+./ex17 db.dat d 2
+
+echo "Listing database contents"
+./ex17 db.dat l
