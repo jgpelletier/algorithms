dd706c1f61aea61c1b21cbdd29c7acb62674bc01
Joshua Pelletier <jgpelletier@gmail.com>
2014-07-09 23:20:52 +0000

    added doubs function

--- ecma/value-v-ref.js
+++ ecma/value-v-ref.js
@@ -1,25 +1,25 @@
 var x = { a: 1 }
 var y = x
 
-console.log(x.a) //<- prints 1
+console.log(x.a) // <- prints 1
 
 y.a = 2
 
-console.log(x.a) //<- prints 2
+console.log(x.a) // <- prints 2
 
 x.a = 1
 
-console.log(y.a) //<- prints 1
+console.log(y.a) // <- prints 1
 
 var x = 1
 var y = x
 
-console.log(y) //<-prints 1
+console.log(y) // <-prints 1
 
 y = 2
 
-console.log(x) //prints 1
-console.log(y) //prints 2
+console.log(x) // prints 1
+console.log(y) // prints 2
 
 function foo (o) {
     o = { a: 2 }
@@ -38,7 +38,7 @@ console.log(o.a) // <- 1
 foo(o)
 console.log('function foo passed a reference to object o evaluates to', o.a) // <- 1
 bar(o)
-console.log('function foo passed a reference to object o evaluates to', o.a) // <- ?
+console.log('function bar passed a reference to object o evaluates to', o.a) // <- ?
 
 function f () {
     var i = 0 // <- only visible to function below
@@ -46,14 +46,17 @@ function f () {
 }
 
 var n = f()
-console.log(n(1)) // <- 1 // <- what are variables that reference functions called in JavaScipt
+console.log(n(1)) // <- 1 // <- what are variables that reference functions called
 console.log(n(1)) // <- 2
 console.log(n(8)) // <- 10
 
 function print (n) { console.log(n(0)) }
 
-function zero (n) { n(-n(0)) }
+function zero (n) { n(-n(0)) }// removing the negative doubles it.
+function doubs (n) { n(n(0)) }
 
+print(n)
+doubs(n)
 print(n)
 zero(n)
 print(n)
