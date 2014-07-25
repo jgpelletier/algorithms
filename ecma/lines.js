3d5bc53f414bc50a35f742e5cb4677244278f8cf
Joshua Pelletier <jgpelletier@gmail.com>
2014-07-25 15:31:28 +0000

    tested async feature

--- ecma/lines.js
+++ ecma/lines.js
@@ -1,6 +1,17 @@
 var fs = require('fs')
 
 // fs.readFile / File System section of the Node.js API docs.
+fs.readFile('wolverine.txt', 'ascii', function(error, buffer) {
+    var line_count = 0
+    for (var i = 0; i < buffer.length; i++) {
+        if (buffer[i] == '\n') {
+            line_count++
+        }
+    }
+    console.log("This is the first function:\nline count: ", line_count, "\ncharacter count: ", i)
+})
+
+
 var buffer = fs.readFileSync('wolverine.txt', 'ascii')
     // ^^ String
 var i = 0
@@ -13,14 +24,4 @@ for (i; i < buffer.length; i++) {
     }
 }
 
-console.log("line count: ", line_count, "\ncharacter count: ", i)
-
-fs.readFile('wolverine.txt', 'ascii', function(error, buffer) {
-    var line_count = 0
-    for (var i = 0; i < buffer.length; i++) {
-        if (buffer[i] == '\n') {
-            line_count++
-        }
-    }
-    console.log("line count: ", line_count, "\ncharacter count: ", i)
-})
+console.log("This is the second function:\nline count: ", line_count, "\ncharacter count: ", i)
