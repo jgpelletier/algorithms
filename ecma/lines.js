0a8d9fd79eee9ec21af06fcb548e70f86ea694b9
Joshua Pelletier <jgpelletier@gmail.com>
2014-07-29 23:48:36 +0000

    implemented lineCount

--- ecma/lines.js
+++ ecma/lines.js
@@ -1,9 +1,10 @@
 var fs = require('fs')
 
-function lineCount (????) { // <- vvvvvv async vvvvvv
+function lineCount (callback) { // <- vvvvvv async vvvvvv
     // fs.readFile / File System section of the Node.js API docs.
     fs.readFile('wolverine.zxt', 'ascii', function (err, buffer) {
-        if (err) throw err
+        if (err) return callback(err)
+        callback(null, buffer)
 
         var line_count = 0
         for (var i = 0; i < buffer.length; i++) {
@@ -12,9 +13,13 @@ function lineCount (????) { // <- vvvvvv async vvvvvv
             }
         }
     })
-} // ^^^^^^ function ^^^^^^^
+}
 
-console.log("This is the first function:\nline count: ", line_count, "\ncharacter count: ", i)
+lineCount(function(err, buffer){
+    console.log(line_count)
+}) // ^^^^^^ function ^^^^^^^
+
+//console.log("This is the first function:\nline count: ", line_count, "\ncharacter count: ", i)
 
 function lineCountSync (filename) {
     var buffer = fs.readFileSync(filename, 'ascii')
@@ -32,4 +37,4 @@ function lineCountSync (filename) {
 }
 
 var line_count = lineCountSync('wolverine.txt')
-console.log("This is the second function:\nline count: ", line_count, "\ncharacter count: ", i)
+//console.log("This is the second function:\nline count: ", line_count, "\ncharacter count: ", i)
