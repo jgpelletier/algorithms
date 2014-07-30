d695c1f9f11f6a4beb8c95565ab8699f002d0ff5
Joshua Pelletier <jgpelletier@gmail.com>
2014-07-30 22:58:50 +0000

    updated lines.js

--- ecma/lines.js
+++ ecma/lines.js
@@ -1,13 +1,18 @@
 var fs = require('fs')
 var line_count = 0
 
-//lineCount(lines)
+
+// Each time LineCount is run the variable line_count is incremented by the
+// number of lines in the file if the line_count variable is not set to 0 within
+// the function.
+//
 function lineCount (callback) { // <- vvvvvv async vvvvvv
     // fs.readFile / File System section of the Node.js API docs.
     fs.readFile('wolverine.txt', 'ascii', function (err, buffer) {
         if (err) throw err;
 
-        //line_count = 0
+        line_count = 0// <- line_count needs to be set within the function
+
         for (var i = 0; i < buffer.length; i++) {
             if (buffer[i] == '\n') {
                 line_count++
@@ -22,13 +27,13 @@ lineCount(function() {
 })
 
 // ^^^^^^ function ^^^^^^^
-/*
+
 function lines() {
     console.log(line_count)
 }
 
 lineCount(lines)
-*/
+
 
 
 /*//console.log("This is the first function:\nline count: ", line_count, "\ncharacter count: ", i)
