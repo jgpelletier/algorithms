2ab10d49d6f1afc7cf0ed662af328fccf2c34f6a
Joshua Pelletier <jgpelletier@gmail.com>
2014-07-30 15:19:51 +0000

    implemented a version of lineCount by pulling the line_count variable to the outer scope

--- ecma/lines.js
+++ ecma/lines.js
@@ -1,26 +1,28 @@
 var fs = require('fs')
 
+var line_count = 0
 function lineCount (callback) { // <- vvvvvv async vvvvvv
     // fs.readFile / File System section of the Node.js API docs.
-    fs.readFile('wolverine.zxt', 'ascii', function (err, buffer) {
-        if (err) return callback(err)
-        callback(null, buffer)
+    fs.readFile('wolverine.txt', 'ascii', function (err, buffer) {
+        if (err) throw (err);
 
-        var line_count = 0
+        //var line_count = 0
         for (var i = 0; i < buffer.length; i++) {
             if (buffer[i] == '\n') {
                 line_count++
             }
         }
-    })
+        callback()
+   })
 }
 
-lineCount(function(err, buffer){
+function lines (){
     console.log(line_count)
-}) // ^^^^^^ function ^^^^^^^
+} // ^^^^^^ function ^^^^^^^
 
+lineCount(lines)
 //console.log("This is the first function:\nline count: ", line_count, "\ncharacter count: ", i)
-
+/*
 function lineCountSync (filename) {
     var buffer = fs.readFileSync(filename, 'ascii')
         // ^^ String
@@ -38,3 +40,4 @@ function lineCountSync (filename) {
 
 var line_count = lineCountSync('wolverine.txt')
 //console.log("This is the second function:\nline count: ", line_count, "\ncharacter count: ", i)
+*/
