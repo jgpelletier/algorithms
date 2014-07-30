0302423b05ff0b037530794130c63420a66fdfca
Joshua Pelletier <jgpelletier@gmail.com>
2014-07-30 20:32:44 +0000

    use an anonymous function

--- ecma/lines.js
+++ ecma/lines.js
@@ -1,28 +1,38 @@
 var fs = require('fs')
-
 var line_count = 0
+
+//lineCount(lines)
 function lineCount (callback) { // <- vvvvvv async vvvvvv
     // fs.readFile / File System section of the Node.js API docs.
     fs.readFile('wolverine.txt', 'ascii', function (err, buffer) {
-        if (err) throw (err);
+        if (err) throw err;
 
-        //var line_count = 0
+        //line_count = 0
         for (var i = 0; i < buffer.length; i++) {
             if (buffer[i] == '\n') {
                 line_count++
             }
         }
         callback()
-   })
+    })
 }
 
-function lines (){
+lineCount(function() {
     console.log(line_count)
-} // ^^^^^^ function ^^^^^^^
+})
+
+// ^^^^^^ function ^^^^^^^
+/*
+function lines() {
+    console.log(line_count)
+}
 
 lineCount(lines)
-//console.log("This is the first function:\nline count: ", line_count, "\ncharacter count: ", i)
-/*
+*/
+
+
+/*//console.log("This is the first function:\nline count: ", line_count, "\ncharacter count: ", i)
+
 function lineCountSync (filename) {
     var buffer = fs.readFileSync(filename, 'ascii')
         // ^^ String
