a0a493081e3c8ff3ab7c7439bb048b24e6f31a93
Joshua Pelletier <jgpelletier@gmail.com>
2014-08-02 02:33:43 +0000

    new assignment

--- ecma/lines-sync.js
+++ ecma/lines-sync.js
@@ -15,5 +15,13 @@ function lineCountSync (filename, i) {
 
 }
 
-var line_count = lineCountSync('wolverine.txt')
-console.log("line count: ", line_count.count , "\ncharacter count: ", line_count.character)
+console.log(process.argv)
+
+// When you get an ENOENT error, print *only*, "error: file not found\n".
+// Any other error, propagate to unhandled exception handler (stack trace dump.)
+try {
+    var line_count = lineCountSync(process.argv[2])
+    console.log("line count: ", line_count.count , "\ncharacter count: ", line_count.character)
+} catch (e) {
+    console.log('I caught an error.')
+}
