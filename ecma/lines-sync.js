51ec95e6d36730f062ebbe260533c4173394ff4f
Joshua Pelletier <jgpelletier@gmail.com>
2014-08-04 14:36:05 +0000

    added addtional function to compare stack trace dump to handled error

--- ecma/lines-sync.js
+++ ecma/lines-sync.js
@@ -19,9 +19,15 @@ console.log(process.argv)
 
 // When you get an ENOENT error, print *only*, "error: file not found\n".
 // Any other error, propagate to unhandled exception handler (stack trace dump.)
+
 try {
-    var line_count = lineCountSync(process.argv[2])
+    var line_count = linecountsync(process.argv[2])
     console.log("line count: ", line_count.count , "\ncharacter count: ", line_count.character)
 } catch (e) {
     console.log('I caught an error.')
 }
+
+
+var line_count = linecountsync(process.argv[2])
+console.log("line count: ", line_count.count , "\ncharacter count: ", line_count.character)
+// ^^ propagates unhandled exception (stack trace dump).
