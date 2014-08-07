ba55e81f498152b0392f7847cd56c841eff1801f
Joshua Pelletier <jgpelletier@gmail.com>
2014-08-07 21:28:27 +0000

    new assignment and lesson

--- ecma/lines-async.js
+++ ecma/lines-async.js
@@ -4,11 +4,15 @@ var line_count
 // Each time LineCount is run the variable line_count is incremented by the
 // number of lines in the file if the line_count variable is not set to 0 within
 // the function.
-//
-function lineCount (callback) { // <- vvvvvv async vvvvvv
+// vvv Not an API function. If you put the English message in here, how do I use
+// your library if my users are French?
+function lineCount (file, callback) { // <- vvvvvv async vvvvvv
     // fs.readFile / File System section of the Node.js API docs.
-    fs.readFile('wolverine.txt', 'ascii', function (err, buffer) {
-        if (err) throw err;
+    console.log('asking for file')
+    fs.readFile(file, 'ascii', function (err, buffer) { // <- here this, a new main
+        // vvv only this is waiting
+        console.log('file is ready', new Error('').stack) // <- that <-
+        if (err) throw err; // <- not created here, NO STACK
 
         line_count = 0// <- line_count needs to be set within the function
 
@@ -17,16 +21,46 @@ function lineCount (callback) { // <- vvvvvv async vvvvvv
                 line_count++
             }
         }
-        callback()
+
+        console.log(new Date)
+        setTimeout(function () {
+            console.log(new Date)
+            callback() // <- call the callback, inclosed in a closure
+        }, 7000)
     })
+    console.log('asked for file', new Error('').stack) // <- this come before ^^^
 }
 
-lineCount(function() {
-    console.log(line_count)
-})
+// -> lineCount(file, callback)
+//
+// If something is wrong, I'll print out a message, laced with profanity, and in
+// Hebrew to stderr and then throw an exception that you can't catch.
+//
+// ^^^ See if that's in Node.js docs. If not, how should it be done?
+//
+//  Google-fu ~ error first callback pattern Node.js
 
-function lines() {
-    console.log(line_count)
+// If it is not there, print the English message.
+// Any other error, throw.
+
+function main (file) { // <- nowhere in a stack
+    lineCount(file, function() {
+        console.log(line_count)
+    })
+
+    function lines() {
+        console.log('lines is called', new Error('').stack) // <- that <-
+        console.log(line_count)
+    }
+
+    lineCount(file, lines)
+
+    console.log('going bye-bye', new Error('').stack)
 }
 
-lineCount(lines)
+main(process.argv[2])
+
+console.log('gone', new Error('').stack)
+
+
+// <- we are gone
