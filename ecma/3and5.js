78a8a94f897bfcfb8dec465c4acd20fca1e9058a
Joshua Pelletier <jgpelletier@gmail.com>
2014-08-12 23:16:03 +0000

    implemented an algorithm to find the sum of the multiples of 3 or 5 below 1000

---
+++ ecma/3and5.js
@@ -0,0 +1,22 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+// get 3, 5, 6 and 9. The sum of these multiples is 23.
+//
+// Find the sum of all the multiples of 3 or 5 below 1000
+
+var i
+var threes = 0
+var fives = 0
+var total
+
+for (i = 0; i < 1000; i++) {
+    if ( i%3 == 0) {
+        threes = i + threes
+    }
+    else if ( i%5 == 0) {
+        fives = i + fives
+    }
+}
+
+total = fives + threes
+
+console.log(total) // 233,168
