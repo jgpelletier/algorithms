85faf70e8b6c42f2c45d8bdad2575e6299cfc9d6
Joshua Pelletier <jgpelletier@gmail.com>
2014-08-13 21:54:59 +0000

    creates linked list out of prime numbers

--- ecma/largestPrimeFactor.js
+++ ecma/largestPrimeFactor.js
@@ -1,19 +1,24 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143?
 //
-// a simple but slow way of tessting for primality consist to testing whether n
+// a simple but slow way of testing for primality consist to testing whether n
 // is a multiple of any number between 2 and square route of n.
 
 
-var i = 1
+var i = 3
 var zeroes = 0
 var x
 var list = {value: null, next: null}
 var node = {}
-var target = 10
+var target = 13195
 
 var sqrRt = Math.sqrt(i)
 var whole = Math.ceil(sqrRt)
+
+function create (list) {
+   var list = { value: null , next: null }
+   return list
+}
 
 function dump (list) {
     var node = list
@@ -25,45 +30,34 @@ function dump (list) {
     }
 }
 
+function push (list, value) {
+    var node = { value: value, next: list.next }
+    list.next = node
+}
+
+list = create(list)
+push(list, 1)
+push(list, 2)
 
 for ( i; i < target; i++) {
-    console.log('\n')
-    console.log('\n1st for loop. i =', i)
     sqrRt = Math.sqrt(i)
     whole = Math.ceil(sqrRt)
 
     x = 1
-    for (x; x <= whole; x++) { // <- the while num
-        console.log('------------')
-        console.log('2nd for loop. x =', x)
+    for (x; x <= i; x++) { // <- the while num
         if (i%x == 0) {
             zeroes++
-            console.log('------------')
-            console.log('\t counting zeroes:', zeroes)
-            console.log('\t square root', sqrRt )
-            console.log('\t whole', whole )
-            if (zeroes >= 3 && x == whole) {
-                console.log('\n')
+            if (zeroes >= 3 || x == whole) {
                 break
             }
-            else if (zeroes == 2 && x == whole) {
-                console.log('------------')
-                console.log('\t counting zeroes:', zeroes)
-                console.log('\t square root', sqrRt )
-                console.log('\t whole', whole )
-                node.value = i
-                console.log('\t node', node.value)
-                console.log('\n')
-            //node.next = list.next
-                //list.next = node
-                break
+            else if (x == i) {
+                push(list, i)
             }
         }
     }
 
     zeroes = 0
-    console.log('------------')
 
 }
 
-//dump(list)
+dump(list)
