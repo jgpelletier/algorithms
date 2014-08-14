9ab849332ad0702853c55f1117dbeac1bd8b18e8
Joshua Pelletier <jgpelletier@gmail.com>
2014-08-14 23:45:22 +0000

    bash script to test lines-async

---
+++ ecma/lines-async-test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+node lines-async.js wolverine.txt french
+node lines-async.js wolverin.txt french
+node lines-async.js gerg.txt french
+node lines-async.js wolverine.txt
+node lines-async.js wolverin.txt
+node lines-async.js gerg.txt
