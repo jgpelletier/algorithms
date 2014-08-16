aa4d2f6716caa380a8786fa7569ee83093a7f697
Joshua Pelletier <jgpelletier@gmail.com>
2014-08-16 23:45:14 +0000

    created for loop the takes an array numbers gets the product

--- ecma/ProjectEuler/3and5.js
+++ ecma/ProjectEuler/3and5.js
@@ -1,7 +1,7 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we
 // get 3, 5, 6 and 9. The sum of these multiples is 23.
 //
-// This program fins the sum of all the multiples of 3 or 5 below the value
+// This program finds the sum of all the multiples of 3 or 5 below the value
 // given by the user
 
 var ceiling = process.argv[2]
