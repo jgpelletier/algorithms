b2352cd36d803b0de8e4faad9be13b18a3493224
Joshua Pelletier <jgpelletier@gmail.com>
2014-09-02 18:06:53 +0000

    made a head node and passed the goEast function a userObject rather then a node

--- ecma/jagged-railway.js
+++ ecma/jagged-railway.js
@@ -5,13 +5,15 @@ function dump (list) {
     console.log(util.inspect(list, null, null))
 }
 
-function goEast (list, node) {
+function goEast (list, object) {
+  var node = { object: object }
+  //var node = object
   var prev = list
   if (!list.east) {
     list.east = node
     node.west = list
   } else {
-    goEast(list.east, node)
+    goEast(list.east, object)
   }
 }
 
@@ -37,23 +39,23 @@ function main () {
     var popped = lines.pop() // <- pops empty line
 
     // We are going to create a linked list.
-    var head
+    var head = { east: null }
 
     // converts the line to an railroad station object and prints the object.
     lines.forEach(function (line) {
         var userObject = object(line) // <- convert the line to a railroad station object
-        var node = { object: userObject }
-        node.east = null
-        node.west = null
+        //var node = { object: userObject }
+        //node.east = null
+        //node.west = null
         // ^^^ userObject is stored in a property of the node.
         //     east and west are appended to the node
 
         // vvv this links the list.
-        if (!head) {
-            head = node
-        } else {
-            goEast(head, node)
-        }
+        //if (!head) {
+        //    head = node
+        //} else {
+            goEast(head, userObject)
+        //}
     })
     dump(head)
 }
