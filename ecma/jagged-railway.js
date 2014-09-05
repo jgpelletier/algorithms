a73309d4ecbae22c54e5a16430c0a12f6f5ac57d
joshua pelletier <jgpelletier@gmail.com>
2014-09-05 11:27:44 -0700

    deleted unnecessary comments

--- ecma/jagged-railway.js
+++ ecma/jagged-railway.js
@@ -1,31 +1,27 @@
 var fs = require('fs')
 var util = require('util')
-var assert = require('assert')
+//var assert = require('assert')
 
-// assertion object?
 
 function dump (list) {
     console.log(util.inspect(list, null, null))
 }
 
-// Split into 2 parts. Create a function called addStation. It knows that it has the 
-// head and tests if east is empty. If empty it will add the node otherwise it will 
-// go east.
 function addStation(list, object) {
     var node = { object: object }
     if (list.east == null) {
         list.east = node 
     }
 }
+// ^^^ for the first node.
 
-function goEast (list, /*count,*/ object) {
+function goEast (list, object) {
     var node = { object: object }
     if (!list.east) {
         list.east = node
         node.west = list
-        //count.push('')
     } else {
-        goEast(list.east, /*count,*/ object)// <- need to keep this recursive function
+        goEast(list.east, object)// <- need to keep this recursive function
     }
 }
 // This ^^^^  creates a function that will walk to
@@ -47,27 +43,14 @@ function object (line) { // function to convert the line to a railroad station o
 function main () {
     var lines = fs.readFileSync(process.argv[2], 'utf8').split('\n')
     var popped = lines.pop() // <- pops empty line
-    //var count = []
     var head  = { east: null } // <- this will never have an object propert.
 
     lines.forEach(function (line) {
         var userObject = object(line)
-        if (head.east == null) addStation(head, userObject) 
-        else goEast(head,/* count,*/ userObject)
+        if (head.east == null) addStation(head, userObject) //  no west property back to the head node.
+        else goEast(head, userObject)
     })
-/*
-    if (!head.east) { // tests if the list is empty
-        console.log('The list is empty.')
-    else if ( count.length != lines.length) { // tests to make sure each line in the file has an object
-        console.log('The list is missing objects')
-    } else {
-        dump(head)
-    }
-*/
 
-
-    //assert.ok(head == true")
-    // ^^ what am I asserting? object is true
     dump(head)
 }
 
