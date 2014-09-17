317b9e0c3102ce1c8942e74863989913aa1fb2a5
Joshua Pelletier <jgpelletier@gmail.com>
2014-09-17 15:14:31 +0000

    need to debug else branch

--- ecma/binarySearchTree.js
+++ ecma/binarySearchTree.js
@@ -73,6 +73,7 @@ function transplant ( head, prev, node, nextNode ) {
     if (node.left) {
         node.right.left = node.left
     }
+
     return head
 }
 
@@ -93,12 +94,19 @@ function deletion (node, value) {
         return transplant( head, prev, node, node.left)
     } else {
         var y = minValue(node.right)
+        y = { object:y }
+        //console.log(y)
         if (prev.right != y) {
-        //transplant(head, prev, node, y)
+            head = transplant(head, prev, node, y)
+            console.log(head)
+            y.left = node.left
+            //y.right = node.right
+
         //prev.left = node.left
         }
+        //y.left = node.left
 
-        return transplant (head, prev, node, node.right)
+        head = transplant (head, prev, y, node.right )
         // console.log(y)
         //prev.right = node.right
         //node.right.left = node.left
