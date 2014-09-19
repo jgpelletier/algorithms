28a798b67b05ced2318616bcbb3332f01218b70f
Joshua Pelletier <jgpelletier@gmail.com>
2014-09-19 00:00:11 +0000

    update

---
+++ c/algorithms/BinaryTreeSearch/bst.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "bst.h"
+
+#define BUFFER_SIZE 1024
---
+++ c/algorithms/BinaryTreeSearch/bst.h
@@ -0,0 +1,16 @@
+struct _line_t {
+    char line[120];
+    struct _line_t*  right;
+    struct _line_t*  left;
+};
+
+typedef struct _line_t lines;
+
+void print_city
+
+// What signitures do I need in here?
+// maxValue
+// minValue
+// search
+// insertion
+// deletion
