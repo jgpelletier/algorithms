95182ea069e7873ec763017924eea4b886cc003b
Joshua Pelletier <jgpelletier@gmail.com>
2014-09-21 19:00:04 +0000

    added np_initialize function and new structs

--- c/algorithms/BinaryTreeSearch/bst.c
+++ c/algorithms/BinaryTreeSearch/bst.c
@@ -3,24 +3,25 @@
 #include <errno.h>
 #include "bst.h"
 
+
+void np_initialize(node_pool_t node_pool, size_t* node_size, void *memory, size_t memory_size);
+{
+    node_pool -> unused = memory;
+    node_pool -> stop = node_pool > unused + memory_size;
+    node_pool -> free_node = NULL;
+    node_pool -> node_size = node_size;
+}
+
 void addValue_array (node *node_pool, int number)
 {
     node_pool-> value = number;
 }
-
-void print (node * list) //definition
-{
-    while (list->right) {
-        printf("value: %d\n", list->right->value);
-        list = list->right;
-    }
-}
-
 /*
-struct head* add(struct head* head, struct node*node)
+void add(node*node_pool, node**node)
 {
-    if (head->right == NULL) {
-        return head->right = node;
+    if (node == NULL) {
+        node->value = &node_pool->value;
+        //printf("%d\n", node->value);
     } else {
        return append(head->right, node);
     }
@@ -32,6 +33,14 @@ struct head* append(struct *head, node *node)
         head->right = node;
     } else {
         return append(head->right, node);
+    }
+}
+
+void print (node * list) //definition
+{
+    while (list->right) {
+        printf("value: %d\n", list->right->value);
+        list = list->right;
     }
 }
 */
--- c/algorithms/BinaryTreeSearch/main.c
+++ c/algorithms/BinaryTreeSearch/main.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "bst.h"
+// understand vvv this function.
+//int compare(const void* a, const void* b) { return (*(int*)a - *(int*)b; }
 
-
+// process node
 int main ()
 {
-    static node node_pool[MAX_NODE]; // here is my pool.
-    int arr[] = { 9, 4, 8, 7, 0, 10, 5, 14, 1, 11, 24, 19, 18, 34, 17 };
-    int i;
+    node_t nodes[MAX_NODE];
+    tree_t tree;
 
+    node_pool_t nodepool;
+
+    np_initialize (&node_pool, sizeof(struct node), nodes, sizeof(nodes));
+
+
+    static int arr[15] = { 9, 4, 8, 7, 0, 10, 5, 14, 1, 11, 24, 19, 18, 34, 17 };
+    int i;
     // values are added to the nodes in the node_pool. this may need to be
     // made a function/
     for ( i = 0; i < MAX_NODE; ++i) {
         addValue_array(&node_pool[i], arr[i]);
-        printf("%d\n", node_pool[i].value);
+        node = &node_pool[i];
     }
-
+    //printf("%d\n," head.right.value);
 
     return 0;
 }
