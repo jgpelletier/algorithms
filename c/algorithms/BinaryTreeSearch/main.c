5d318e127f728e6f0e251c30d45bbd88d725dca1
Joshua Pelletier <jgpelletier@gmail.com>
2014-10-10 23:57:54 +0000

    update

--- c/algorithms/BinaryTreeSearch/main.c
+++ c/algorithms/BinaryTreeSearch/main.c
@@ -13,9 +13,9 @@ int main ()
 
     np_pool_t node_pool;
     
-    //initializeTree(tree);
+   // initializeTree(tree);
 
-    np_initialize (&node_pool, sizeof(node_t), nodes, sizeof(nodes));
+    np_initialize(&node_pool, sizeof(node_t), nodes, sizeof(nodes));
 
 
     static int arr[15] = { 9, 4, 8, 7, 0, 10, 5, 14, 1, 11, 24, 19, 18, 34, 17 };
@@ -23,7 +23,7 @@ int main ()
     // values are added to the nodes in the node_pool. this may need to be
     // made a function/
     for ( i = 0; i < MAX_NODE; ++i) {
-        add_node(tree, np_allocate(&node_pool), &arr[i]);
+        add_node(&tree, np_allocate(&node_pool), &arr[i]);
     }
 
     return 0;
