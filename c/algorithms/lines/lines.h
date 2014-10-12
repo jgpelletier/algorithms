5760308386ddb888c86bcd2719a61dc31c103b55
Joshua Pelletier <jgpelletier@gmail.com>
2014-10-12 16:28:09 +0000

    Reformatted pointers for consistency.

--- c/algorithms/lines/lines.h
+++ c/algorithms/lines/lines.h
@@ -30,12 +30,12 @@ struct _file_info2 *share_info2 (int lines, size_t length);
 struct _file_info3 share_info3 (int lines, size_t length);
 struct _file_info *line_count (const char* fname);
                 // ^^^ using line_count_3
-void line_count_2 (const char* fname, struct _file_info2** info2, int* error);
-void line_count_4 (const char* fname, int *lines, int *length, int *error);
-void line_count_3 (const char* fname, struct _file_info3* info3, int* error);
-struct _file_info3 line_count_5 (const char* fname, int* error);
+void line_count_2 (const char *fname, struct _file_info2 **info2, int *error);
+void line_count_4 (const char *fname, int *lines, int *length, int *error);
+void line_count_3 (const char *fname, struct _file_info3 *info3, int *error);
+struct _file_info3 line_count_5 (const char *fname, int *error);
                                                 // ^^^ using line_count_3
-int read_lines_fgets (const char* fname, struct _line_t *lines);
-int read_lines (const char* fname, struct _line_t *lines);
+int read_lines_fgets (const char *fname, struct _line_t *lines);
+int read_lines (const char *fname, struct _line_t *lines);
 void print_lines(struct _line_t *lines);
 void delete_lines (struct _line_t *lines);
