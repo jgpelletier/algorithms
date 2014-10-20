811e63072ec22a6de0e361e9c1d9476d3581f431
Joshua Pelletier <jgpelletier@gmail.com>
2014-10-20 21:50:51 +0000

    Fixed pointer formatting.

--- c/algorithms/compile/compile.c
+++ c/algorithms/compile/compile.c
@@ -4,7 +4,7 @@ static int i = 0; // <- static is local to `compile.c`.
 
 char greeting[] = "Hello, World!\n";
 
-int* get_number(const char* message) // <- definition
+int *get_number(const char *message) // <- definition
 {
     printf(message);
     return &i;  // <- body
