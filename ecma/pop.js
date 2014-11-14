ceee60e57148003a9234ad6dedb059dfa2065023
joshua pelletier <jgpelletier@gmail.com>
2014-11-14 15:30:14 -0800

    Removed comment.

--- ecma/pop.js
+++ ecma/pop.js
@@ -3,5 +3,4 @@ var data = fs.readFileSync(process.argv[2])
 var array = data.toString().split('\n') //splits lines into array elements
 var popped = array.pop()
 var ppp = array.pop()
-// * End your confusion. The final string in the array is empty.
 console.log(ppp)
