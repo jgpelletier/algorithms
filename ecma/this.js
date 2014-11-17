43d71d8419638ecb33a790b72ae02f7c17dfc9d8
joshua pelletier <jgpelletier@gmail.com>
2014-11-17 15:19:11 -0800

    Added comments on this precedence.

--- ecma/this.js
+++ ecma/this.js
@@ -164,4 +164,25 @@ goodBaz()
 //    object. Hence, `this` is not always meaningful, but it is always
 //    there.
 //    
-//    IS NODE ALWAYS IN STRICT MODE?
+//    `this` Precedence: Taken from You don't know JS
+//    the rules for determining this from a function call's call-site, in their order of precedence.
+//    Ask these questions in this order, and stop when the first rule applies.
+//
+//    Is the function called with new (new binding)? If so, this is the newly constructed object.
+//
+//    var bar = new foo()
+//
+//    Is the function called with call or apply (explicit binding), even hidden inside a bind hard binding?
+//    If so, this is the explicitly specified object.
+//
+//    var bar = foo.call( obj2 )
+//
+//    Is the function called with a context (implicit binding), otherwise known as an owning or containing
+//    object? If so, this is that context object.
+//
+//    var bar = obj1.foo()
+//
+//    Otherwise, default the this (default binding). If in strict mode, pick undefined, otherwise pick the
+//    global object.
+//
+//    var bar = foo()
