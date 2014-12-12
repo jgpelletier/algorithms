cc18602d4395e89149920b92f27156f4c0cba7c3
joshua pelletier <jgpelletier@gmail.com>
2014-12-11 16:56:02 -0800

    Initial commit.

---
+++ ecma/factorial.js
@@ -0,0 +1,22 @@
+
+function fact_recursive (n) {
+    if (n == 0) return 1
+    else return n * fact_recursive(n-1)
+}
+
+function fact_cps (n, ret) {
+    if (n == 0) {
+        ret(1)
+    } else {
+        fact_cps(n-1, function (t0) {
+            ret(n * t0) 
+         })
+    }
+}
+
+var factorial = fact_recursive(4)
+console.log("recursive " + factorial)
+
+fact_cps(4, function(n) {
+    console.log( "cps " + n)
+})
