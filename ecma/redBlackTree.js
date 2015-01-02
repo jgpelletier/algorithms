b3e59e36b497af8bc32efe7dd3614fb0b5a0742e
Joshua Pelletier <jgpelletier@gmail.com>
2015-01-02 14:34:56 -0800

    Add insert-fixup psuedo code.

--- ecma/redBlackTree.js
+++ ecma/redBlackTree.js
@@ -19,3 +19,26 @@
 // RB-INSERT-FIXUP.T; ́/
 
 // Make RB insert in JavaScript
+
+/*
+
+B-INSERT-FIXUP(T,z)
+while z.p.color == RED 
+    if z.p == z.p.p.left
+    y = z.p.p.right 
+    if y.color == RED
+        z.p.color = BLACK 
+        y.color = BLACK 
+        z.p.p.color = RED 
+        z = z.p.p
+    else if z == z.p.right 
+            z = z.p
+            LEFT-ROTATE(T, z)
+        z.p.color = BLACK
+        z.p.p.color = RED
+        RIGHT-ROTATE(T,z.p.p)
+     else (same as then clause
+     with “right” and “left” exchanged)
+ T.root.color = BLACK
+
+*/
