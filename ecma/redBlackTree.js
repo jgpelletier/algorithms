d64ef8035e2a27c3970f6d89fb752108782a7ee9
Joshua Pelletier <jgpelletier@gmail.com>
2015-01-03 14:39:27 -0800

    Fix format.

--- ecma/redBlackTree.js
+++ ecma/redBlackTree.js
@@ -1,22 +1,25 @@
 // This is the psuedo code taken from introduction to algorithms
-// RB-INSERT(T,z)
-// y = T.nil
-// x = T.root 
-//   whilex¤T.nil
-//      y = x
-//      if z.key < x:key
-//          x = x.left 
-//      else x = x.right
-// z.p = y 
-// if y== T.nil
-//     T.root = z 
-// elseif z.key < y.key 
-//     y.left = z
-// else y.right = z
-// z.left =  T.nil
-// z.right = T.nil
-// z.color = RED 
-// RB-INSERT-FIXUP.T; ́/
+
+/*
+RB-INSERT(T,z)
+y = T.nil
+x = T.root 
+while x != T.nil
+    y = x
+    if z.key < x:key
+        x = x.left 
+    else x = x.right
+z.p = y 
+if y== T.nil
+    T.root = z 
+elseif z.key < y.key 
+    y.left = z
+else y.right = z
+z.left =  T.nil
+z.right = T.nil
+z.color = RED 
+RB-INSERT-FIXUP(T,z)
+*/
 
 // Make RB insert in JavaScript
 
