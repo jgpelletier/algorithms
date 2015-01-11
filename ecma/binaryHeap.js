df3ae9a4f09700a88555d8c05366ac28e1c70bf1
Joshua Pelletier <jgpelletier@gmail.com>
2015-01-10 16:13:28 -0800

    Test `child_of` and `parent_of`.

--- ecma/binaryHeap.js
+++ ecma/binaryHeap.js
@@ -3,3 +3,13 @@
 var parent_of = function (m) { var i = Math.floor(m/2); return m % 2 ? i : i - 1
 }
 var child_of = function (n, which) { return n * 2 + which }
+
+
+console.log(child_of(0,1))
+console.log(child_of(0,2))
+console.log(child_of(1,1))
+console.log(child_of(1,2))
+console.log(parent_of(4))
+console.log(parent_of(3))
+console.log(parent_of(2))
+console.log(parent_of(1))
