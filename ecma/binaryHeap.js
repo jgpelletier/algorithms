7acd4d1d03498122c11eac4aa9ca49a110a34eaa
Joshua Pelletier <jgpelletier@gmail.com>
2015-01-12 16:17:35 -0800

    Remove test functoins.

--- ecma/binaryHeap.js
+++ ecma/binaryHeap.js
@@ -4,12 +4,3 @@ var parentOf = function (m) { var i = Math.floor(m/2); return m % 2 ? i : i - 1
 }
 var childOf = function (n, which) { return n * 2 + which }
 
-
-console.log(child_of(0,1))
-console.log(child_of(0,2))
-console.log(child_of(1,1))
-console.log(child_of(1,2))
-console.log(parent_of(4))
-console.log(parent_of(3))
-console.log(parent_of(2))
-console.log(parent_of(1))
