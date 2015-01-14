9bf0d29d4e90706555ad508857633fbb245ca6ac
Joshua Pelletier <jgpelletier@gmail.com>
2015-01-13 16:20:58 -0800

    Add comments.

--- ecma/binaryHeap.js
+++ ecma/binaryHeap.js
@@ -4,3 +4,7 @@ var parentOf = function (m) { var i = Math.floor(m/2); return m % 2 ? i : i - 1
 }
 var childOf = function (n, which) { return n * 2 + which }
 
+
+
+// add a push function and a shift function.
+// add a max_heapify and a min_heapify.
