29d27179063001f210976002c22a85b296d840d4
Joshua Pelletier <jgpelletier@gmail.com>
2015-01-14 16:29:19 -0800

    Add test.

--- ecma/binaryHeap.js
+++ ecma/binaryHeap.js
@@ -8,3 +8,19 @@ var childOf = function (n, which) { return n * 2 + which }
 
 // add a push function and a shift function.
 // add a max_heapify and a min_heapify.
+
+
+var a = []
+
+a.push(3)
+a.push(4)
+a.push(6)
+a.push(7)
+a.push(1)
+
+console.log
+// Why doesn't this vvv work?
+console.log(childOf(0,1))
+var b = a[childOf(0,1)]
+console.log(a[childOf(0,1)])
+console.log(b)
