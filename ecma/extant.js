4e6369e267dab4c04b0322dffb973fe079e8d5a0
Joshua Pelletier <jgpelletier@gmail.com>
2015-05-11 17:48:49 -0700

    Correct answers.

--- ecma/extant.js
+++ ecma/extant.js
@@ -2,6 +2,6 @@ function isTruthy (condition) { return !! condition }
 
 var object = {}
 
-console.log(isTruthy(object.next === undefined)) // <-- truthy? yes
-console.log(isTruthy(object.next == undefined)) // <- truthy? yes
-console.log(isTruthy(object.next)) // <- truthy? no
+console.log(isTruthy(object.next === undefined)) // <-- truthy? true
+console.log(isTruthy(object.next == undefined)) // <- truthy? true
+console.log(isTruthy(object.next)) // <- truthy? false
