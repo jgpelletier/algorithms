f2f87b115895766a0a91e388a7502bd6407c1a97
Joshua Pelletier <jgpelletier@gmail.com>
2015-05-15 20:18:09 -0700

    Fix psuedocode.

--- ecma/redBlack/redBlackTree.js
+++ ecma/redBlack/redBlackTree.js
@@ -202,29 +202,29 @@ RB-TRANSPLANT(T, u, v)
 
 /*
 RB-DELETE-FIXUP.T; x/
-1 while x ¤ T:root and x:color == BLACK
-2 if x == x:p:left
-3 w D x:p:right
-4 if w:color = = RED
-5 w:color D BLACK // case 1
-6 x:p:color D RED // case 1
-7 LEFT-ROTATE.T; x:p/ // case 1
-8 w D x:p:right // case 1
-9 if w:left:color == BLACK and w:right:color = = BLACK
-10 w:color D RED // case 2
-11 x D x:p // case 2
-12 else if w:right:color == BLACK
-13 w:left:color D BLACK // case 3
-14 w:color D RED // case 3
-15 RIGHT-ROTATE.T; w/ // case 3
-16 w D x:p:right // case 3
-17 w:color D x:p:color // case 4
-18 x:p:color D BLACK // case 4
-19 w:right:color D BLACK // case 4
-20 LEFT-ROTATE.T; x:p/ // case 4
-21 x D T:root // case 4
-22 else (same as then clause with “right” and “left” exchanged)
-23 x:color D BLACK
+1 while x != T.root && x.color == BLACK
+2 if x == x.p.left
+3   w = x.p.right
+4   if w.color == RED
+5       w.color = BLACK // case 1
+6       x.p.color = RED // case 1
+7       LEFT-ROTATE.T; x.p/ // case 1
+8       w = x.p.right // case 1
+9       if w.left.color == BLACK && w.right.color == BLACK
+10          w.color = RED // case 2
+11          x =  x.p // case 2
+12      else if w.right.color == BLACK
+13          w.leftr.color D BLACK // case 3
+14          w.color D RED // case 3
+15          RIGHT-ROTATE(T,w)/ // case 3
+16           w = x.p.right // case 3
+17      w.color = x.p.color // case 4
+18      x.p.color = BLACK // case 4
+19      w.right.color = BLACK // case 4
+20      LEFT-ROTATE.T; x:p/ // case 4
+21      x = T.root // case 4
+22  else (same as then clause with “right” and “left” exchanged)
+23 x.color D BLACK
 */
 
 
