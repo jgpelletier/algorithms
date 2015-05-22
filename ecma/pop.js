86faccd662bdab807a169484373e5cac8293be54
Joshua Pelletier <jgpelletier@gmail.com>
2015-05-21 18:33:51 -0700

    Fix variable.

--- ecma/pop.js
+++ ecma/pop.js
@@ -2,5 +2,5 @@ var fs = require('fs')
 var data = fs.readFileSync(process.argv[2])
 var array = data.toString().split('\n') //splits lines into array elements
 var popped = array.pop()
-var ppp = array.pop()
-console.log(ppp)
+var pop = array.pop()
+console.log(pop)
