1440f9333c0c7a5e2adba0298532333892af8da2
Joshua Pelletier <jgpelletier@gmail.com>
2015-06-06 15:43:09 -0300

    Add comments about Unicode.

--- ecma/airplane.js
+++ ecma/airplane.js
@@ -1,3 +1,6 @@
-// Unicode example.
+// Unicode example. Unicode accounts for different langauges and characters in a
+// way that ascii does not.
+
+// the unicode numbers below will print airplane in arabic.
 var airplane = { arabic: '\u0637\u0681\u06CC\u0631\u0629',  manufacturer: "Boeing" }
 console.log("Unicode example: ", airplane)
