98a46b8a1621a0195af404afd76cb415c53e818a
Joshua Pelletier <jgpelletier@gmail.com>
2015-06-08 15:49:26 -0300

    Add comment.

--- ecma/arrayLesson.js
+++ ecma/arrayLesson.js
@@ -1,3 +1,5 @@
+// Finish describing the array methods.
+
 var fs = require('fs')
 var data = fs.readFileSync(process.argv[2])
 var array = data.toString().split('\n') // splits lines into array elements
